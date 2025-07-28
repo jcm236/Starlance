a2c5f28dbfec65691ada4f3db6d8e5c0e80df715
zyxkad <zyxkad@gmail.com>
2025-07-28 12:14:50 -0600

    fix dismount to

--- src/main/java/net/jcm/vsch/mixin/minecraft/MixinServerPlayer.java
+++ src/main/java/net/jcm/vsch/mixin/minecraft/MixinServerPlayer.java
@@ -4,9 +4,21 @@ import net.minecraft.server.level.ServerPlayer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ServerPlayer.class)
 public abstract class MixinServerPlayer extends MixinPlayer {
+	@ModifyArg(
+		method = "dismountTo",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;setPos(DDD)V"),
+		index = 1
+	)
+	public double dismountTo$setPos$y(double y) {
+		if (this.vsch$shouldFreeRotate()) {
+			final float oldHeight = this.vsch$getVanillaDimensions(this.getPose()).height;
+			final float newHeight = 0.6f;
+			y += oldHeight - newHeight;
+		}
+		return y;
+	}
 }
