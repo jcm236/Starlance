418a5bda979699549c6b897d618aa5f2c387843f
zyxkad <zyxkad@gmail.com>
2025-10-16 18:06:41 -0600

    bump version to v1.4.0

--- src/main/java/net/jcm/vsch/config/VSCHCommonConfig.java
+++ src/main/java/net/jcm/vsch/config/VSCHCommonConfig.java
@@ -5,18 +5,17 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class VSCHCommonConfig {
-    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static final ForgeConfigSpec SPEC;
+	public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+	public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.ConfigValue<Boolean> DISABLE_LOD_WARNING;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> DISABLE_LOD_WARNING;
 
+	static {
+		DISABLE_LOD_WARNING = BUILDER.comment("Disables the warning about lodDetail being below 4096. Its highly recommend you increase lod instead of using this option").define("disable_lod_warning", false);
+		SPEC = BUILDER.build();
+	}
 
-    static {
-        DISABLE_LOD_WARNING = BUILDER.comment("Disables the warning about lodDetail being below 4096. Its highly recommend you increase lod instead of using this option").define("disable_lod_warning", false);
-        SPEC = BUILDER.build();
-    }
-
-    public static void register(ModLoadingContext context){
-        context.registerConfig(ModConfig.Type.COMMON, VSCHCommonConfig.SPEC, "vsch-common.toml");
-    }
+	public static void register(ModLoadingContext context){
+		context.registerConfig(ModConfig.Type.COMMON, VSCHCommonConfig.SPEC, "vsch-common.toml");
+	}
 }
