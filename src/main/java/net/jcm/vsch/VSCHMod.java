8d8e7eba19888b7c44d4cc4893e673263ba9d783
zyxkad <zyxkad@gmail.com>
2025-12-16 11:30:28 -0700

    register attachments
    Co-authored-by: jcm236 <johnchristi236@gmail.com>

--- src/main/java/net/jcm/vsch/VSCHMod.java
+++ src/main/java/net/jcm/vsch/VSCHMod.java
@@ -41,6 +41,9 @@ public class VSCHMod {
 		VSCHTab.register(modBus);
 		VSCHTags.register();
 
+		ValkyrienSkiesMod.getApi().registerAttachment(ShipLandingAttachment.class);
+		ValkyrienSkiesMod.getApi().registerAttachment(VSCHForceInducedShips.class);
+
 		// Register commands (I took this code from another one of my mods, can't be bothered to make it consistent with the rest of this)
 		MinecraftForge.EVENT_BUS.register(ModCommands.class);
 
