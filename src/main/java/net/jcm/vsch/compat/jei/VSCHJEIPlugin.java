d92a03f0c4163841b3c856658c66b8cd26cdb8ce
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2026-02-28 16:31:04 -0500

    Add JEI plugin
    Fixes the tier blocks not showing up in JEI

---
+++ src/main/java/net/jcm/vsch/compat/jei/VSCHJEIPlugin.java
@@ -0,0 +1,21 @@
+package net.jcm.vsch.compat.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IExtraIngredientRegistration;
+import net.jcm.vsch.blocks.VSCHBlocks;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+@JeiPlugin
+public class VSCHJEIPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath("vsch", "jei_plugin");
+    }
+
+    @Override
+    public void registerExtraIngredients(IExtraIngredientRegistration registration) {
+        registration.addExtraItemStacks(VSCHBlocks.TIER_BLOCKS.stream().map(reg -> new ItemStack(reg.get().asItem())).toList());
+    }
+}
