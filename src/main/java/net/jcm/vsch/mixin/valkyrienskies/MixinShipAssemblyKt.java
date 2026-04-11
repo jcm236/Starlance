b4e21ce83a921aee1bbdd5ed993cf1cda6e5379b
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2026-04-11 00:07:29 -0400

    Fix assembly mixin

--- src/main/java/net/jcm/vsch/mixin/valkyrienskies/MixinShipAssemblyKt.java
+++ src/main/java/net/jcm/vsch/mixin/valkyrienskies/MixinShipAssemblyKt.java
@@ -15,17 +15,16 @@
  **/
 package net.jcm.vsch.mixin.valkyrienskies;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.valkyrienskies.core.api.ships.ServerShip;
-import org.valkyrienskies.core.util.datastructures.DenseBlockPosSet;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
-import org.valkyrienskies.mod.common.assembly.ShipAssemblyKt;
 
 import net.jcm.vsch.VSCHMod;
 import net.jcm.vsch.config.VSCHServerConfig;
@@ -33,21 +32,24 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import org.valkyrienskies.mod.common.assembly.ShipAssembler;
 
-@Mixin(ShipAssemblyKt.class)
+import java.util.Set;
+
+@Mixin(ShipAssembler.class)
 public class MixinShipAssemblyKt {
 	@Unique
 	private static final Logger LOGGER = LogManager.getLogger(VSCHMod.MODID);
 
 	// Goofy ahhh temporary fix but it'll atleast help out the kids who don't know not to do this
-	@Inject(method = "createNewShipWithBlocks", at = @At("HEAD"), remap = false, cancellable = true)
-	private static void createNewShipWithBlocks(BlockPos centerBlock, DenseBlockPosSet blocks, ServerLevel level, CallbackInfoReturnable<ServerShip> cir) {
-		// If block is higher than overworld height
-		if (centerBlock.getY() > VSGameUtilsKt.getYRange(level).getMaxY()) {
+	@Inject(method = "assembleToShipFull", at = @At(value = "INVOKE", target = "Lorg/valkyrienskies/core/internal/world/VsiServerShipWorld;createNewShipAtBlock(Lorg/joml/Vector3ic;ZDLjava/lang/String;)Lorg/valkyrienskies/core/api/ships/ServerShip;"), remap = false, cancellable = true)
+	private static void assembleToShipFull(ServerLevel level, Set<? extends BlockPos> blocks, double scale, CallbackInfoReturnable<ShipAssembler.AssembleContext> cir, @Local(name = "fromCenter") Vector3d fromCenter) {
+        // If block is higher than overworld height
+		if (fromCenter.y() > VSGameUtilsKt.getYRange(level).getMaxY()) {
 			if (VSCHServerConfig.CANCEL_ASSEMBLY.get()) {
 				level.getServer().getPlayerList().broadcastSystemMessage(
 					Component.literal("Starlance: Multi-block assembly above world height, cancelling. Instead, use ship creator stick, or assemble in another dimension. You can override this behavior in config, but its not recommended.").withStyle(ChatFormatting.RED), false);
-				LOGGER.warn("Starlance cancelled multi-block assembly above overworld build height. You can override this behavior in config, but its not recommended.");
+				LOGGER.warn("Starlance cancelled multi-block assembly above overworld build height. You can override this behavior in config, but it's not recommended.");
 				cir.cancel();
 			} else {
 				LOGGER.warn("Multi-block assembly above build height NOT cancelled by starlance: be warned");
